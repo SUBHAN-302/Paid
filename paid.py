29910dcab17ff0e09bf681f1e1b96a0e1ae4ee61
SUBHAN-302 <110691961+SUBHAN-302@users.noreply.github.com>
2022-11-06 20:25:18 +0500

    Add files via upload

---
+++ paid.py
@@ -0,0 +1,9 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import subhan1()
